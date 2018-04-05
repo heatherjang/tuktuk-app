7759ce88c53df0da6a353e3b75670cb1f6e015f3
Heather Jang <heather.m.jang@gmail.com>
2018-04-05 16:14:30 -0700

    Set up 2 routes for testing

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
-const express = require('express');
-const path = require('path');
-const serveStatic = require('serve-static');
+const express      = require('express');
+const path         = require('path');
+const serveStatic  = require('serve-static');
+const logger       = require('morgan');
+const bodyParser   = require('body-parser');
+const cookieParser = require('cookie-parser');
+// Database
+const mongo       = require('mongodb');
+const monk        = require('monk');
+const mongoUri    = 'mongodb://sandboxMaster:masterpass@ds125489.mlab.com:25489/sandbox'
+const db          = monk(mongoUri)
+
+// // routes
+// const routes = require('./routes/index');
+// const users  = require('./routes/users');
 
 let app = express();
+
 app.use(serveStatic(__dirname + "/dist"));
 
+// app.get('/', (req, res) => {
+//   res.send(
+//     [{
+//       title: "Hello World!",
+//       description: "Hi there! How are you? toay"
+//     }]
+//   )
+// })
+
 const port = process.env.PORT || 8080;
+
 app.listen(port, () => {
   console.log('Listening on port ' + port)
 });
